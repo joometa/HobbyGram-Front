e6cb7afa18497a850bc47691e2c18d05e6072220
pmk2424 <pmk4236@naver.com>
2021-04-12 23:01:57 +0900

    mainpage Update

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { Route } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configureStore";
+
+import Main from "../pages/Main";
+import Header from "../components/Header";
+
 function App() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Header></Header>
+      <ConnectedRouter history={history}>
+        <Route path="/" exact component={Main} />
+      </ConnectedRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
