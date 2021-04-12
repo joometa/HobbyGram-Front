2652182d650f7a9aec898d6ac6256efd2ea4cb27
Jeongjoo Lee <61656046+JeongJoo-Lee@users.noreply.github.com>
2021-04-12 16:27:48 +0900

    프로젝트 시작전 기본셋팅

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "../shared/App";
+import "./shared/index.css";
+import App from "./shared/App";
 
 import reportWebVitals from "./reportWebVitals";
 
