da1249b85f75fa46e357dcf562675a6e79fb6597
pmk2424 <pmk4236@naver.com>
2021-04-20 19:47:44 +0900

    헤더 닉네임 css 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -133,9 +133,10 @@ const Font = styled.div`
 `;
 
 const Nickname = styled.div`
-  width: 100px;
+  width: 200px;
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
   margin-right: 10px;
+  align-items: center;
 `;
